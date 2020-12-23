c89f2ced9876ca46dc2796c7288a25f235cd23c1
ftp.vio1ence <76502917+vio1ence@users.noreply.github.com>
2020-12-23 04:53:23 +0300

    Add files via upload

---
+++ ClassLibrary1/SaveLog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ClassLibrary1
+{
+   public class SaveLogs
+    {
+        public event Action<string> message;
+        public string Path;
+        public SaveLogs() { }
+
+        public SaveLogs(string path)
+        {
+            Path = path;
+        }
+
+
+        public async void Add(string message)
+        {
+            if (Path != null)
+            {
+                using (FileStream file = new FileStream($@"{Path}\logs.txt", FileMode.Append))
+                {
+                    byte[] array = System.Text.Encoding.Default.GetBytes($"{message}\n");
+                    await file.WriteAsync(array, 0, array.Length);
+                }
+                this.message?.Invoke("Сохранение прошло успешно");
+            }
+            else
+            {
+                using (FileStream file = new FileStream("log.txt", FileMode.Append))
+                {
+                    byte[] array = System.Text.Encoding.Default.GetBytes($"{message}\n");
+                    await file.WriteAsync(array, 0, array.Length);
+                }
+                this.message?.Invoke("Лог сохранён по стандартному пути");
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary1;
+using System;
+using static System.Console;
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Введите текст чтобы записать лог:");
+            string message = ReadLine();
+            WriteLine(message);
+            SaveLogs log = new SaveLogs();
+            log.message += (mes) => WriteLine(mes);
+            log.Add(message);
+        }
+    }
+}
